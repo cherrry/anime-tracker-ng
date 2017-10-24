aa4fcfb17cd7b96c7626168da4e59c61f500666a
Cherry Ng <cherry92@gmail.com>
2017-10-24 20:05:25 +0800

    Initialize with React and AntD.

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import React from 'react';
+import { render } from 'react-dom';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <Button>Hello World</Button>
+  );
+}
+
+render(<App />, document.getElementById('app-root'));
