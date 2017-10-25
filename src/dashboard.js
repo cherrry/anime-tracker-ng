c562faa932f895d0348384335fc04f53645f2b08
Cherry Ng <cherry92@gmail.com>
2017-10-25 19:59:25 +0800

    Use English as default locale.

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { LocaleProvider } from 'antd';
+import EnUS from 'antd/lib/locale-provider/en_US';
 
 import Main from 'dashboard/main';
 
-render(<Main />, document.getElementById('app-root'));
+function App() {
+  return (
+    <LocaleProvider locale={EnUS}>
+      <Main />
+    </LocaleProvider>
+  );
+}
+
+render(<App />, document.getElementById('app-root'));
