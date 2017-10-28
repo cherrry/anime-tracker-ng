1113ea887a42476abc732176cdaf28efad4fb0d2
Cherry Ng <cherry92@gmail.com>
2017-10-28 17:42:25 +0800

    Webtorrent player that supposingly work.

--- src/dashboard/modals/watch-anime-modal.js
+++ src/dashboard/modals/watch-anime-modal.js
@@ -21,40 +21,21 @@ class WatchAnimeModal extends Component {
   }
 
   componentDidMount() {
-    /* eslint-disable */
     this.bt = new WebTorrent();
-    console.log(WebTorrent.WEBRTC_SUPPORT);
-    console.log(this.bt);
-    console.log(this.props.torrentLink);
-    this.bt.on('error', (err) => {
-      console.error(err);
-    });
     this.bt.add(this.props.torrentLink, (torrent) => {
-      console.log(torrent);
       const file = torrent.files.find((file) => {
-        console.log(file);
         return file.name.endsWith('.mp4');
       });
-      file.getBlobURL((err, url) => {
-        this.setState({
-          blobUrl: url,
-        });
-      });
+      file.appendTo(this.video);
     });
-    /* eslint-enable */
   }
 
   componentWillUnmount() {
-    /* eslint-disable */
-    this.bt.destroy(() => {
-      console.log('destroyed');
-    });
-    /* eslint-enable */
+    this.bt.destroy();
   }
 
   render() {
     const {title, label, onCancel} = this.props;
-    const {blobUrl} = this.state;
     return (
       <Modal
         visible={true}
@@ -63,7 +44,7 @@ class WatchAnimeModal extends Component {
         footer={null}
         maskClosable={false}
       >
-        {blobUrl}
+        <div ref={(video) => this.video = video} />
       </Modal>
     );
   }
